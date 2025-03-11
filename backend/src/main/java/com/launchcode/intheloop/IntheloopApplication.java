dcee677227aa63169c74e1b05723334cecd1a81e
Pranusha <narsicloud@gmail.com>
2025-03-10 22:05:53 -0500

    created backend folder structure

---
+++ backend/src/main/java/com/launchcode/intheloop/IntheloopApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.intheloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntheloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntheloopApplication.class, args);
+	}
+
+}
