d54bbd272f0464f578b5226dea018b06a193d523
chisschwartz <christopherschwartz559@gmail.com>
2025-03-15 14:01:55 -0500

    Built up comment model class, and integrated with post model class

--- backend/src/main/java/com/launchcode/intheloop/models/Comment.java
+++ backend/src/main/java/com/launchcode/intheloop/models/Comment.java
@@ -1,7 +1,22 @@
 package com.launchcode.intheloop.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
-public class Comment {
+public class Comment extends AbstractEntity {
+
+    @Size(min = 1, message = "Comment cannot be blank")
+    public String comment;
+
+    @ManyToOne
+    private Post post;
+
+    public Comment(){}
+
 }
--- backend/src/main/java/com/launchcode/intheloop/models/Post.java
+++ backend/src/main/java/com/launchcode/intheloop/models/Post.java
@@ -1,9 +1,13 @@
 package com.launchcode.intheloop.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,6 +16,10 @@ public class Post extends AbstractEntity {
 
     @ManyToOne
     private WatchList watchList;
+
+    @JoinColumn(name = "post_id")
+    @OneToMany
+    private final List<Comment> comments = new ArrayList<>();
 
     public Post(){}
 }
