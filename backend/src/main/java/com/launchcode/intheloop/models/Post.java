72edd5365e8a87dc4c16eb4e843a87258ae89eae
chisschwartz <christopherschwartz559@gmail.com>
2025-03-15 13:50:59 -0500

    intialized some of post to make a connection to watchlist

--- backend/src/main/java/com/launchcode/intheloop/models/Post.java
+++ backend/src/main/java/com/launchcode/intheloop/models/Post.java
@@ -1,7 +1,17 @@
 package com.launchcode.intheloop.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
-public class Post {
+public class Post extends AbstractEntity {
+
+    @ManyToOne
+    private WatchList watchList;
+
+    public Post(){}
 }
