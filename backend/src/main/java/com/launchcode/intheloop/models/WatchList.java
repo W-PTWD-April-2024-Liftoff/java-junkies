499a0b7ae6bf69f250df4d811bb4b682c7d0cfed
chisschwartz <christopherschwartz559@gmail.com>
2025-03-15 13:51:58 -0500

    built up watchlist model

--- backend/src/main/java/com/launchcode/intheloop/models/WatchList.java
+++ backend/src/main/java/com/launchcode/intheloop/models/WatchList.java
@@ -1,7 +1,27 @@
 package com.launchcode.intheloop.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
-public class WatchList {
+public class WatchList extends AbstractEntity{
+
+    //need to ask about annotations
+    //could be integer
+    public String savedPosts;
+
+    //may need a list here to store posts
+    @JoinColumn(name = "watchlist_id")
+    @OneToMany
+    private final List<Post> posts = new ArrayList<>();
+
+    public WatchList(){}
 }
