28f105e34eb66007f0244141940ae6874a853e35
chisschwartz <christopherschwartz559@gmail.com>
2025-03-15 13:38:21 -0500

    added abstract entity cllass

---
+++ backend/src/main/java/com/launchcode/intheloop/models/AbstractEntity.java
@@ -0,0 +1,34 @@
+package com.launchcode.intheloop.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    //equals and hash for id in lombok annotations @Getter @Setter
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
