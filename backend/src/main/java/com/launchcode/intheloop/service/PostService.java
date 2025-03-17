c65679d7468022364b95c9febcf4796e50a99947
Pranusha <narsicloud@gmail.com>
2025-03-17 10:18:42 -0500

    added and updated Post service

--- backend/src/main/java/com/launchcode/intheloop/service/PostService.java
+++ backend/src/main/java/com/launchcode/intheloop/service/PostService.java
@@ -4,9 +4,11 @@ import com.launchcode.intheloop.data.PostRepository;
 import com.launchcode.intheloop.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -27,4 +29,28 @@ public class PostService {
         return postRepository.save(post);
     }
 
+    public void deletePostById(Long id){
+        Optional<Post> results = postRepository.findById(id);
+
+        if(results.isEmpty()){
+            throw new NoSuchElementException("Post not found with id "+ id);
+        }
+
+        Post post = results.get();
+        postRepository.delete(post);
+    }
+
+    public Post updatePostById(Long id, Post updatedPost){
+        Optional<Post> results = postRepository.findById(id);
+
+        if(results.isEmpty()){
+            throw new NoSuchElementException("Post not found with id "+ id);
+        }
+
+        Post post = results.get();
+        post.setTitle(updatedPost.getTitle());
+        post.setContent(updatedPost.getContent());
+
+        return postRepository.save(post);
+    }
 }
