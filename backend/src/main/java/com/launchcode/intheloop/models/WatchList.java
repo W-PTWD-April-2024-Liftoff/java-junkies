41a520b2eee6a437e75d7019c512447100069c70
chisschwartz <christopherschwartz559@gmail.com>
2025-03-18 19:51:24 -0500

    watchlist controller created and service refactored

--- backend/src/main/java/com/launchcode/intheloop/models/WatchList.java
+++ backend/src/main/java/com/launchcode/intheloop/models/WatchList.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Entity
 public class WatchList extends AbstractEntity{
 
+    public String title;
     public String savedPosts;
 
     //may need a list here to store posts
--- backend/src/main/java/com/launchcode/intheloop/service/WatchListService.java
+++ backend/src/main/java/com/launchcode/intheloop/service/WatchListService.java
@@ -1,11 +1,13 @@
 package com.launchcode.intheloop.service;
 
 import com.launchcode.intheloop.data.WatchListRepository;
+import com.launchcode.intheloop.models.Comment;
 import com.launchcode.intheloop.models.WatchList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -14,17 +16,43 @@ public class WatchListService {
     @Autowired
     private WatchListRepository watchListRepository;
 
+    public WatchList createWatchList(WatchList watchList){
+        if(watchList.getSavedPosts() != null){
+            watchList.setSavedPosts(watchList.getSavedPosts());
+        }
+        return watchListRepository.save(watchList);
+    }
+
     public Iterable<WatchList> getAllWatchList() {
         return watchListRepository.findAll();
     }
 
-    //id doesn't work for some reason
-    public Optional<WatchList> getWatchListById (Long id) {
+    public Optional<WatchList> getWatchListById(Long id) {
         return watchListRepository.findById(id);
     }
 
-    public WatchList createWatchList (WatchList watchList) {
-            return watchListRepository.save(watchList);
+    public void deleteWatchListById(Long id) {
+        Optional<WatchList> results = watchListRepository.findById(id);
+
+        if (results.isEmpty()) {
+            throw new NoSuchElementException("No watchlist with id: " + id);
+        }
+
+        WatchList watchList = results.get();
+        watchListRepository.delete(watchList);
+    }
+
+    public WatchList updateWatchListById(Long id, WatchList updatedWatchlist) {
+        Optional<WatchList> results = watchListRepository.findById(id);
+
+        if(results.isEmpty()) {
+            throw new NoSuchElementException("No watchlist with id: " + id);
+        }
+        WatchList watchList = results.get();
+        watchList.setTitle(updatedWatchlist.getTitle());
+        watchList.setSavedPosts(updatedWatchlist.getSavedPosts());
+
+        return watchListRepository.save(watchList);
     }
 
 }
