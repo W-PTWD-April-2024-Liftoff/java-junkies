2e7038a016f86ace46ac63c4b754cf581e24c377
chisschwartz <christopherschwartz559@gmail.com>
2025-03-26 17:04:01 -0500

    more watchlist functionality done

--- frontend/src/components/WatchList.jsx
+++ frontend/src/components/WatchList.jsx
@@ -35,11 +35,29 @@ export const ShareProvider = ({ children }) => {
         localStorage.setItem(LocalStorageKey, JSON.stringify(watchListPosts));
         }, [watchListPosts]);
 
+//clears the whole watchlist
     const clearWatchlist = () => {
         localStorage.removeItem(LocalStorageKey);
         setWatchListPosts([]);
         };
 
-    const
+//removes a specific post from the watchlist, updating the state by filtering out the post with it's postSlug
+    const removeWatchList = (postSlug) => {
+        setWatchListPosts((prevWatchList) =>
+            prevWatchList.filter((post) => post.slug !== postSlug)
+            );
+        };
+
+//value provided to SharedContext.Provider contains watchListPosts, with addWatchList, removeWatchList, and clearWatchlist.
+//Makes it accessible to other components with useContext.
+//SharedProvider contains all logic for managing watchlist and provides data to child components with SharedContext.Provider
+//Should make it so we do not hav eto pass props manually
+    return (
+        <SharedContext.Provider
+            value = {{ watchListPosts, addWatchList, removeWatchList, clearWatchlist }}
+        >
+        {children}
+        </SharedContext.Provider>
+        );
 
     };
