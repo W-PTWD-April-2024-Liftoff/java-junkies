24d3f9e13f61ccb24c541f0f0633f2882af47c69
chisschwartz <christopherschwartz559@gmail.com>
2025-04-11 21:49:01 -0500

    dark mode is realgit add . on the homepage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,12 +1,19 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./HomePage.css";
 
 const HomePage = () => {
+
+  const darkMode = () => {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
 
   return (
     <>
     <div>
+    <button onClick={darkMode}>Prefer dark mode?</button>
       <h1>Welcome to In The Loop</h1>
       <p>
         <Link to={`/user/login`}>Login</Link>
