ff4a7ec12b55544457938aae740380f3fd63ac20
chisschwartz <christopherschwartz559@gmail.com>
2025-04-11 21:29:28 -0500

    small changes

---
+++ frontend/src/components/CreateComment.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const CreateComment = ({onCommentCreated}) => {
+    const [text, setText] = useState("");
+    const [createdAt, setCreatedAt] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const commentData = {
+            text,
+            createdAt
+        };
+
+     try{
+        const response = await fetch("http://localhost8080/comments", {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify(commentData)
+        });
+        
+     if(!response.ok) {
+        throw new Error('HTTP error! Status: ${response.status}');
+     }
+     
+     const data = await response.json();
+     console.log("comment created:", data);
+     onCommentCreated();
+     setText("");
+     setCreatedAt("");
+     } catch (error) {
+        console.error("Error creating comment:", error);
+     }
+    };
+
+    return (
+        <form>
+        <input type="text" placeholder="Add a comment..." value={text} onChange={(e) => setText(e.target.value)} required/>        
+        <textarea placeholder="Add a comment..." value={text} onChange={(e) => setText(e.target.value)} required/>
+        <button type="submit">Create Comment</button>
+        </form>
+    );
+ 
+};
+
+export default CreateComment;
